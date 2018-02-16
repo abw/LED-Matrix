e477f9b2319ddadcd20ce3e3536698f60254193b
Andy Wardley <abw@wardley.org>
2018-02-16 07:29:37 +0000

    Tidied up some comments, disabled some debugging

--- arduino/LED-Demo1/LED-Demo1.ino
+++ arduino/LED-Demo1/LED-Demo1.ino
@@ -4,6 +4,8 @@
 #include "Cursor.hpp"
 #include "Button.hpp"
 
+#define DEBUG_SERIAL 0
+
 // Define the LED matrix as (width, height)
 const unsigned int ledPin  = 12;
 const unsigned int width   = 24;
@@ -11,7 +13,6 @@ const unsigned int height  = 18;
 const unsigned int numLeds = width * height;
 CRGB leds[width * height];
 LEDMatrix matrix(leds, width, height, BottomRight, RowsSnake);
-TrailBlazer blazer(&matrix);
 
 // Define the four buttons with integrated LEDs, each has a switch pin
 // (input) and LED pin (output).  The latter (i.e. 3, 5, 6 and 9) must
@@ -21,7 +22,7 @@ TrailBlazer blazer(&matrix);
 // The fifth argument indicates if the button is active.
 Button redButton(2, 3, 0.7, 0, false);
 Button greenButton(4, 5, 0.6, 0, true);
-Button blueButton(7, 6, 1.0, 0, false);  // yes, these pins are not in the logical order!
+Button blueButton(7, 6, 1.0, 0, false);  // yes, these pins are supposed to be out of order!
 Button whiteButton(8, 9, 1.0, 0, false);
 
 // create a list of pointers to the buttons so we can iterate over them
@@ -38,18 +39,21 @@ Cursor cursor(
     Reflect, Reflect,   // edge handling x, y
     48                  // trail length
 );
+// a trailblazer is a bit of code to render the cursor trail to the LEDs
+TrailBlazer blazer(&matrix);
+
 // I can't think of a better name for this variable that determines the
 // maximum speed of the cursor.  The maximum range of movement (pixels
 // per second) is this factor multipled by the width and/or height as
 // appropriate.  This is then shift down half way to give a negative
-// or positive range of half that value.  So for a 24 x 18 array, the
-// maximum range would be -72 to +72 in the x direction and -54 to +54
-// in the y direction.
+// or positive range of half that value.  So for a 24 x 18 array with
+// maxFactor set to 6, the maximum range would be -72 to +72 (3 x 24 = 72)
+// in the x direction and -54 to +54 (3 x 18)  in the y direction.
 const unsigned int maxFactor = 6;
 
 // global state variables
-unsigned long start;      // millis
-double time;            // floating point seconds
+unsigned long start;      // milliseconds elapsed
+double time;              // floating point seconds
 bool running = false;
 
 // function prototypes;
@@ -71,12 +75,14 @@ void setup() {
     blackout();
 
     // In my implementation the unit goes into standby mode when first
-    // turned on with only the green button active (see third arguments
-    // where buttons are created above.  When the green button is pressed
+    // turned on with only the green button active (see fifth arguments
+    // where buttons are created above).  When the green button is pressed
     // the other buttons are activated and the LED patterns display.
     greenButton.onPress(startup);
+#if DEBUG_SERIAL
     Serial.begin(9600);
     Serial.println("setup");
+#endif
 }
 
 void startup(Button *b) {
@@ -94,7 +100,9 @@ void startup(Button *b) {
     running = true;
     start = millis();
     time = 0;
+#if DEBUG_SERIAL
     Serial.println("startup");
+#endif
 }
 
 void loop() {
@@ -134,19 +142,27 @@ void blackout() {
 }
 
 void pushRed(Button *b) {
+#if DEBUG_SERIAL
     Serial.println("Push Red");
+#endif
 }
 
 void pushGreen(Button *b) {
+#if DEBUG_SERIAL
     Serial.println("Push Green");
+#endif
 }
 
 void pushBlue(Button *b) {
+#if DEBUG_SERIAL
     Serial.println("Push Blue");
+#endif
 }
 
 void pushWhite(Button *b) {
+#if DEBUG_SERIAL
     Serial.println("Push White");
+#endif
     cursor.changeDirection(
         (double) (rand() % (width  * maxFactor)) - (width  * maxFactor / 2),
         (double) (rand() % (height * maxFactor)) - (height * maxFactor / 2)
